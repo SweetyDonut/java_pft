331d1ff57d488ae817f9e9e42b62cfea135cd99e
SweetyDonut <danilbabin@mail.ru>
2017-05-06 19:20:18 +0300

    V 1.2.5 Реализована функция distance

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -14,11 +14,20 @@ class MyFirstProgram
         Rectangle r = new Rectangle(4,6);
         System.out.println("Площадь прямоугольника со сторонами " +r.a+" и "+r.b+" = " + r.area());
 
+        Point p1= new Point(0,1);
+        Point p2= new Point(0,2);
+
+        System.out.println(distance(p1,p2));
+
     }
 
     public static void hello (String somebody){
 
         System.out.println("Hello,"+ somebody+ "!");
+    }
+
+    public static double distance(Point p1, Point p2){
+        return Math.sqrt((p2.x-p1.x)*(p2.x-p1.x)+(p2.y-p1.y)*(p2.y-p1.y));
     }
 
 
