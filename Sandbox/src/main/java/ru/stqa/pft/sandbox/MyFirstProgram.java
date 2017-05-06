99578a21b0f7f00b3448008080cb2603cbb1b189
SweetyDonut <danilbabin@mail.ru>
2017-05-06 18:51:20 +0300

    V 1.2.2 Добавлены некоторые функции

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,7 +4,30 @@ class MyFirstProgram
 {
     public static void main(String args[]){
 
-        System.out.println("Hello, World!");
+        hello("world");
+        hello("user");
+        hello("Alexei");
 
-        }
+        double l = 8;
+        System.out.println("Площадь квадрата со стороной" +l+" = " + area(l));
+
+        double a = 4;
+        double b = 6;
+
+        System.out.println("Площадь прямоугольника со сторонами" +a+" и "+b+" = " + area(a,b));
+
+    }
+
+    public static void hello (String somebody){
+
+        System.out.println("Hello,"+ somebody+ "!");
+    }
+
+    public static double area(double len){
+        return len*len;
+    }
+
+    public static double area(double a,double b){
+
+    }
 }
