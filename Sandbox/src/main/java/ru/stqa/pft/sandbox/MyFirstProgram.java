c6f561a0fb957127fd613c72fb7c812c7f1ba8ce
SweetyDonut <danilbabin@mail.ru>
2017-05-06 19:12:16 +0300

    V 1.2.3 Добавлен некоторый функционал
    Добавлены классы Square и Reqtangle а также реализованы функции для них.

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -8,13 +8,11 @@ class MyFirstProgram
         hello("user");
         hello("Alexei");
 
-        double l = 8;
-        System.out.println("Площадь квадрата со стороной" +l+" = " + area(l));
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " +s.l+" = " + s.area() );
 
-        double a = 4;
-        double b = 6;
-
-        System.out.println("Площадь прямоугольника со сторонами" +a+" и "+b+" = " + area(a,b));
+        Rectangle r = new Rectangle(4,6);
+        System.out.println("Площадь прямоугольника со сторонами " +r.a+" и "+r.b+" = " + r.area());
 
     }
 
@@ -23,11 +21,7 @@ class MyFirstProgram
         System.out.println("Hello,"+ somebody+ "!");
     }
 
-    public static double area(double len){
-        return len*len;
-    }
 
-    public static double area(double a,double b){
-        return a*b;
-    }
+
+
 }
