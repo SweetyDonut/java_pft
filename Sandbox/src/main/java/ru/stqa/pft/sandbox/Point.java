c806371620223ce3fc4493f657b79d5781bd691b
SweetyDonut <danilbabin@mail.ru>
2017-05-06 19:14:41 +0300

    V 1.2.4 Создан класс Point

---
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Даниил on 06.05.2017.
+ */
+public class Point {
+  double x;
+  double y;
+
+  public Point(int x, int y){
+    this.x = x;
+    this.y = y;
+  }
+}
