a215db3c1d64e62bdb75dac85cc5038641643242
SweetyDonut <danilbabin@mail.ru>
2017-05-21 04:31:02 +0300

    V 3.1.3 Оптимизирован выбор браузера(== заменен на Equals())

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
-
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -28,11 +27,11 @@ public class ApplicationManager {
 
 
   public void init() {
-    if (browser==BrowserType.FIREFOX){
+    if (browser.equals(BrowserType.FIREFOX)){
       wd = new FirefoxDriver();
-    }else if(browser==BrowserType.CHROME){
+    }else if(browser.equals(BrowserType.CHROME)){
       wd = new ChromeDriver();
-    }else if(browser==BrowserType.IE){
+    }else if(browser.equals(BrowserType.IE)){
       wd=new InternetExplorerDriver();
     }
 
