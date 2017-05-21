eac4b4d1a1dcd5cc16320bedb4dc47a6c953bfca
SweetyDonut <danilbabin@mail.ru>
2017-05-21 20:44:12 +0300

    V 4.1.1 Класс Collections

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
@@ -1,9 +1,29 @@
 package ru.stqa.pft.sandbox;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by Даниил on 21.05.2017.
  */
 public class Collections {
 
+  public static void main(String[] args) {
+    String[] langs = {"java","c#","Pyton","PHP"};
+    List<String> languages = Arrays.asList("java","c#","Pyton","PHP");
 
+    for (String l: languages) {
+      System.out.println("Я хочу выучить"+ l);
+    }
+
+    for (String l: langs) {
+      System.out.println("Я хочу выучить"+ l);
+    }
+
+    for (int i = 0; i <languages.size() ; i++) {
+
+      System.out.println("Я хочу выучить"+ languages.get(i));
+    }
+  }
 }
