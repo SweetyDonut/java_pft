f41e14c6676791f6038ba7cacd743a928b375534
SweetyDonut <danilbabin@mail.ru>
2017-05-21 21:09:17 +0300

    V 4.1.2 Добавлена проверка соответствия количества групп в начале и конце теста

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -7,12 +8,14 @@ public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() {
-
     app.getNavigationHelper().goToGroupPage();
+    int before = app.getGroupHelper().getGroupCount();
     app.getGroupHelper().initGroupCreation();
     app.getGroupHelper().fillGroupForm(new GroupData("test3", null, "test2"));
     app.getGroupHelper().submitGroupCreation();
     app.getGroupHelper().returnToGroupPage();
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after,before+1);
   }
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDelitionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDelitionTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 public class GroupDelitionTests extends TestBase {
@@ -8,12 +9,22 @@ public class GroupDelitionTests extends TestBase {
     @Test
     public void testGroupDelition() {
         app.getNavigationHelper().goToGroupPage();
+
+        int before = app.getGroupHelper().getGroupCount();
+
         if (!app.getGroupHelper().isThereAGroup()){
             app.getGroupHelper().createGroup(new GroupData("test1","test1","test1"));
         }
+
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().deleteSelectedGroup();
         app.getNavigationHelper().goToGroupPage();
+        int after = app.getGroupHelper().getGroupCount();
+        if (before!=0){
+            Assert.assertEquals(after,before-1);
+        }
+        else Assert.assertEquals(after, before);
+
     }
 
 }
