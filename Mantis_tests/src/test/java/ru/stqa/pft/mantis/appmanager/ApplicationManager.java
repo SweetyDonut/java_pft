4f636b3d441b8e19f4065254ee2dfdc196047f23
SweetyDonut <danilbabin@mail.ru>
2017-06-12 18:20:38 +0300

    V 8.1.1 Добавлена инфраструктура для тестирования нового приложения

--- Mantis_tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ Mantis_tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -1,5 +1,5 @@
 package ru.stqa.pft.mantis.appmanager;
-
+//
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -31,7 +31,6 @@ public class ApplicationManager {
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
-
 
     if (browser.equals(BrowserType.FIREFOX)) {
       wd = new FirefoxDriver();
