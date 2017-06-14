acd00a55a4c71d1de04fc6e45fc94ce6d82ecd56
SweetyDonut <danilbabin@mail.ru>
2017-06-14 20:48:57 +0300

    V 9.1 ошибка. не генерится классы вебсервиса

---
+++ soap-sample/src/test/java/ru/stqa/pft/soap/GeoIpServiceTests.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.soap;
+
+import org.testng.annotations.Test;
+
+
+/**
+ * Created by Даниил on 14.06.2017.
+ */
+
+public class GeoIpServiceTests {
+ /* @Test
+  public void testMyIp{
+    //new GeoIpService.
+  }*/
+
+}
